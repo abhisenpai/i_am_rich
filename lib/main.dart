aac552aaaf9a389bd7318499be15c2567e4d6bbb
root <root@localhost.localdomain>
2020-04-28 13:16:29 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Image(
+            image:
+                AssetImage('images/joker.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
